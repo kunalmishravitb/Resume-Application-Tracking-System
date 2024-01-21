cd90e5f705860d82f697bbec3ffd0182938b4702
kunalmishravitb <kunal.mishra2021@vitbhopal.ac.in>
2024-01-22 02:25:29 +0530

    third commit

---
+++ app2.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import google.generativeai as genai
+import os
+import PyPDF2 as pdf
+from dotenv import load_dotenv
+
+load_dotenv() # Load all our environment variables from .env file
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+# Create our function
+def get_gemini_response(input):
+    model=genai.GenerativeModel('gemini-pro')
+    response=model.generate_content(input)
+    return response.text
+
+
+# Function to read all the text from the uploaded file
+def input_pdf_text(uploaded_file):
+    reader=pdf.PdfReader(uploaded_file)
+    text=""
+    for page in range(len(reader.pages)): # reader.pages will explore each and every pages inside this
+        # From every page we will try to read the text information
+        page=reader.pages[page]
+        text+=str(page.extract_text()) # From this page we convert into a string and read all the information by using extract_text functionality and all this information will be appended to a text variable
+    return text
+
+
+# Creating a Prompt Template
+input_prompt="""
+            Hey Act like a skilled or very experienced ATS (Application Tracking System)
+            with a deep understanding of tech field, software engineering, data science, data analyst
+            and big data engineer. Your task is to evaluate the resume based on the given job description.
+            You must consider the job market is very competitive and you should provide
+            best assistance for improving the resumes. Assign the percentage matching based
+            on Job Description (jd) and
+            the missing keywords with high accuracy
+            resume:{text}
+            description:{jd}
+
+            I want the response in one single string having the structure
+            {{"JD Match":"%","MissingKeywords:[]","Profile Summary":""}}
+
+            """
+
+# Streamlit App
+st.title("Smart ATS")
+st.text("Improve Your Resume ATS")
+jd=st.text_area("Paste the Job Description")
+uploaded_file=st.file_uploader("Upload Your Resume",type="pdf",help="Please Upload the PDF")
+
+submit=st.button("Submit")
+
+if submit:
+    if uploaded_file is not None:
+        text=input_pdf_text(uploaded_file)
+        response=get_gemini_response(input_prompt)
+        st.subheader(response)
+
